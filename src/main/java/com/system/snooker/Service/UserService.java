dedc2db230492270ff4f3e6345167fd148cfd39d
suyogyaady <suyogyaadi@gmail.com>
2023-08-10 17:25:19 +0545

    navigate configuration, fonts,

--- src/main/java/com/system/snooker/Service/UserService.java
+++ src/main/java/com/system/snooker/Service/UserService.java
@@ -6,25 +6,14 @@ import com.system.snooker.entity.User;
 import java.util.List;
 
 public interface UserService {
-
-
-
-
     String save(UserPojo userPojo);
-
     User findByEmail(String email);
-
     List<User> fetchAll();
-
     User fetchById(Integer id);
-
     void deleteById(Integer id);
-
     String updateResetPassword(String email);
 
-
     void processPasswordResetRequest(String email);
-
     void sendEmail();
 }
 
